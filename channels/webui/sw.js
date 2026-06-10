e97092cc5feded458ad4df3763e0c04c47c65189
Rose22 <rosemelody22@gmail.com>
2026-06-10 04:08:03 +0200

    pesky sw.js

--- channels/webui/sw.js
+++ channels/webui/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'openlumara-v4.9.6';
+const CACHE_NAME = 'openlumara-v4.9.7';
 const ASSETS = ['/', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
